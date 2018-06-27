7075128a9d09e693532b7693d99cf9f2197663e6
Andrew Connelly <andrew.connelly@spgusa.com>
2018-06-27 13:04:50 -0400

    Add project files.

---
+++ Employee/Employee.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace Employee
+{
+    class Employee
+    {
+        private string firstName, lastName;
+        private decimal monthSalary;
+
+        public string FirstName
+        {
+            get { return firstName; }
+            set { firstName = value; }
+        }
+        public string LastName{
+            get { return lastName; }
+            set { lastName = value; }
+        }
+        public decimal MonthlySalary
+        {
+            get { return monthSalary; }
+            set { if (value <= 0M) throw new ArgumentOutOfRangeException("Value cannot be negative.");
+                  else monthSalary = value; }
+        }
+        
+        public Employee(string constFirstName, string constLastName, decimal constMonthlySalary) {
+            firstName = constFirstName;
+            lastName = constLastName;
+            monthSalary = constMonthlySalary;
+        }//end constructor
+
+        public decimal IncreaseSalary(decimal salary) {
+            decimal adjustedSalary;
+            adjustedSalary = salary * 1.1M;
+            return adjustedSalary;
+        }//end method
+
+        public decimal AnnualSalary(decimal salary) {
+            decimal annualSalary;
+            annualSalary = salary * 12;
+            return annualSalary;
+        }//end salary
+
+
+        
+    }//end class
+}//end namespace
---
+++ Employee/EmployeeTest.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Employee
+{
+    class EmployeeTest
+    {
+            public static void Main(string [] args) {
+            Employee Andrew = new Employee("Andrew", "Connelly", 2000.00M);
+            Employee Rebecca = new Employee("Rebecca", "Connelly", 1600.00M);
+
+            decimal salaryIncreaseAndrew = Andrew.IncreaseSalary(Andrew.MonthlySalary);
+            decimal salaryIncreaseRebecca = Rebecca.IncreaseSalary(Rebecca.MonthlySalary);
+
+            decimal annualSalaryAndrew = Andrew.AnnualSalary(Andrew.MonthlySalary);
+            decimal annualSalaryRebecca = Rebecca.AnnualSalary(Rebecca.MonthlySalary);
+
+           
+            Console.WriteLine("Andrew's monthly salary after increase is: {0:C}, his annual salary is: {1:C}", salaryIncreaseAndrew, annualSalaryAndrew);
+            Console.WriteLine("Rebecca's monthly salary after increase is: {0:C}, her annual salary is: {1:C}", salaryIncreaseRebecca, annualSalaryRebecca);
+            Console.ReadKey();
+            }//end main method
+                
+    }//end class
+}//end namespace
